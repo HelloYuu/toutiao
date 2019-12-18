be138e2c022f4556079c9551b1d93011c388f9b6
wang <734455123@qq.com>
2019-12-18 21:44:27 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,57 +1,94 @@
 <template>
-<div class="login">
-<el-card class="loginbox">
-    <div class="title">
-        <img src="../../assets/img/logo_admin.png" alt="hehe">
-    </div>
+  <div class="login">
+    <el-card class="loginbox">
+      <div class="title">
+        <img src="../../assets/img/logo_admin.png" alt="hehe" />
+      </div>
 
-    <el-form>
-        <el-form-item>
-            <el-input placeholder="请输入手机号"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-input style="width:70%" placeholder="请输入验证码"></el-input>
-            <el-button plain style="float:right">发送验证码</el-button>
-        </el-form-item>
-        <el-form-item>
-            <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="success" style="width:100%">登录</el-button>
-        </el-form-item>
-    </el-form>
+      <el-form ref="myForm" :model="loginForm" :rules="loginRules">
 
-</el-card>
-</div>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:70%" placeholder="请输入验证码"></el-input>
+          <el-button plain style="float:right">发送验证码</el-button>
+        </el-form-item>
+
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意
+              <el-link type="primary">用户协议</el-link>和
+              <el-link type="primary">隐私条款</el-link>
+            </el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="success" style="width:100%" @click="onSubmit" >登录</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '呵呵' },
+          { pattern: /^1[3456789]\d{9}$/, message: 'hehe' }
+        ],
+        code: [{ required: true, message: '哈哈' },
+          { pattern: /^\d{6}$/, message: 'haha' }
+        ],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('同意吗？'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$refs.myForm.validate(function (isOK) {
+        console.log('haha')
+      })
+    }
+  }
 
 }
 </script>
 
 <style lang="less" scoped>
-.login{
-    background-image: url('../../assets/img/login_bg.jpg');
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .loginbox {
+    width: 580px;
+    height: 350px;
+    background-color: rgba(255, 255, 255, 0.3);
+    border: none;
+    .title {
+      display: flex;
+      justify-content: center;
+      img {
+        margin-bottom: 20px;
 
-    .loginbox{
-        width: 580px;
-        height: 350px;
-        background-color: rgba(255, 255, 255, 0.3);
-        border: none;
-        .title{
-            display: flex;
-            justify-content: center;
-                img{
-                    margin-bottom: 20px;
-                }
-        }
-
+      }
     }
+  }
 }
 </style>
