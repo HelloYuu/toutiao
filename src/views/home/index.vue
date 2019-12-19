286b3d6a5de70b4236bb0f0db2df164678abb74c
wang <734455123@qq.com>
2019-12-19 17:36:20 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="login">
-      <span>主页</span>
-  </div>
+  <el-container> <!-- 外层容器  -->
+
+    <el-aside><!-- 侧边栏容器  -->
+    侧边栏容器
+    </el-aside>
+
+    <el-container><!-- 内层容器  -->
+
+      <el-header><!-- 顶栏容器  -->
+        顶栏容器
+      </el-header>
+
+      <el-main><!-- 主要区域容器-->
+        内容
+      </el-main>
+
+    </el-container>
+
+  </el-container>
 </template>
 
 <script>
