bf7bc73a3b07dfe243c71f54ce961c5ba03a43e1
wang <734455123@qq.com>
2019-12-21 17:01:43 +0800

    接口访问-axios拦截器-统一处理请求token

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 格式化
-import axios from 'axios'
+import axios from './utils/request'
 import Component from './components'
 import './permision'
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
