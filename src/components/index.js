fa1e6af06ddbde90f4f27ae1c44b4ada678a289d
wang <734455123@qq.com>
2019-12-21 19:40:13 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHesder from './home/layout-header'
+import BreadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHesder)
+    Vue.component('bread-crumb', BreadCrumb)
   }
 }
