73c66feda32ad2689c54595a7c3e42d6f04faec7
wang <734455123@qq.com>
2019-12-21 16:37:36 +0800

    主页模块-导航守卫

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 格式化
 import axios from 'axios'
 import Component from './components'
-
+import './permision'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(Component)
+
 new Vue({
   router,
   render: h => h(App)
