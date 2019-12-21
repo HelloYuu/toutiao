44a457df0f1c3842f40005dd540b8a3ec869dee7
wang <734455123@qq.com>
2019-12-21 17:53:32 +0800

    接口访问-axios拦截器-统一处理异常数

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import router from '../router'
+import { Message } from 'element-ui'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 axios.interceptors.request.use(function (config) {
   let token = window.localStorage.getItem('user-token')
@@ -9,7 +11,32 @@ axios.interceptors.request.use(function (config) {
 })
 axios.interceptors.response.use(function (response) {
   return response.data ? response.data : {}
-}, function () {
-
+}, function (error) {
+  let status = error.response.status // 获取失败的状态码
+  let message = '未知错误'
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      router.push('/login')
+      break
+    case 507:
+      message = '服务器数据库异常'
+      break
+    case 401:
+      message = 'token过期或未出'
+      window.localStorage.clear() // 清空缓存
+      router.push('/login') // this.$router.push()
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  Message({ type: 'warning', message })
+  //   希望 在异常处理函数中将所有的错误都处理完毕 不再进入catch  终止错误
+  return new Promise(function () {}) // 终止当前的错误
 })
 export default axios
