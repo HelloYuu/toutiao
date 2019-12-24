68379d9bad63e6982e8d2be564db8149bef114e2
wang <734455123@qq.com>
2019-12-24 21:33:10 +0800

    素材列表-素材的上传

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
       <bread-crumb slot="header">
       <template slot="title">素材管理</template>
       </bread-crumb>
+      <el-row type="flex" justify="end">
+          <el-upload :http-request="uploadImg" :show-file-list="false">
+             <el-button size="small" type="primary">点击上传</el-button>
+          </el-upload>
+      </el-row>
       <el-tabs v-model="activeName" @tab-click="changeTab">
           <el-tab-pane label="全部素材" name="all">
             <div class="img-list">
@@ -14,12 +19,12 @@
                     </el-row>
                 </el-card>
             </div>
-            <el-row type="flex" justify="center" style="height:50px; margin-top:40px" align="middler">
+            <el-row type="flex" justify="center" style="height:50px;margin-top:30px" align="middler">
                 <el-pagination background layout="prev, pager, next"
                 :total="page.total"
                 :current-page="page.currentPage"
                 :page-size="page.pageSize"
-                @current-change="changePage" >
+                @current-change="changePage">
                 </el-pagination>
             </el-row>
           </el-tab-pane>
@@ -29,14 +34,14 @@
                     <img :src="item.url" alt="">
                 </el-card>
             </div>
-            <el-row type="flex" justify="center" style="height:50px; margin-top:40px" align="middler">
+            <el-row type="flex" justify="center" style="height:50px; margin-top:30px" align="middler">
                     <el-pagination background layout="prev, pager, next"
                     :total="page.total"
                     :current-page="page.currentPage"
                     :page-size="page.pageSize"
                     @current-change="changePage" >
                     </el-pagination>
-                </el-row>
+            </el-row>
           </el-tab-pane>
       </el-tabs>
   </el-card>
@@ -46,6 +51,7 @@
 export default {
   data () {
     return {
+      loading: false,
       activeName: 'all',
       list: [],
       page: {
@@ -56,6 +62,19 @@ export default {
     }
   },
   methods: {
+    uploadImg (params) {
+      this.loading = true
+      let form = new FormData()
+      form.append('image', params.file)
+      this.$axios({
+        method: 'post',
+        url: '/user/images',
+        data: form
+      }).then(result => {
+        this.loading = false
+        this.getAllMaterial()
+      })
+    },
     changePage (newPage) {
       this.page.currentPage = newPage
       this.getAllMaterial()
@@ -67,9 +86,11 @@ export default {
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect',
+        params: {
+          collect: this.activeName === 'collect',
           page: this.page.currentPage,
-          per_page: this.page.pageSize }
+          per_page: this.page.pageSize
+        }
       }).then(result => {
         this.list = result.data.results
         this.page.total = result.data.total_count
