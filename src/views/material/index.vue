1278f8eebd8aaa0bad76b1451a341bca504cebda
wang <734455123@qq.com>
2019-12-24 16:18:26 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,16 +3,25 @@
       <bread-crumb slot="header">
       <template slot="title">素材管理</template>
       </bread-crumb>
-      <el-tabs>
-          <el-tab-pane label="全部素材"></el-tab-pane>
-          <el-tab-pane label="收藏素材"></el-tab-pane>
+      <el-tabs v-model="activeName">
+          <el-tab-pane label="全部素材" name="all">
+32
+          </el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect">
+              12
+          </el-tab-pane>
       </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'collect'
+    }
+  },
+  mounted: {}
 }
 </script>
 
