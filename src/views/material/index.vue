eae0dc509116aef1d38fa0c698e7cb3f7da22b1c
wang <734455123@qq.com>
2019-12-24 20:28:31 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,12 +3,40 @@
       <bread-crumb slot="header">
       <template slot="title">素材管理</template>
       </bread-crumb>
-      <el-tabs v-model="activeName">
+      <el-tabs v-model="activeName" @tab-click="changeTab">
           <el-tab-pane label="全部素材" name="all">
-32
+            <div class="img-list">
+                <el-card class="img-card" v-for="item in list" :key="item.id">
+                    <img :src="item.url" alt="">
+                    <el-row class="operate" type="flex" justify="space-around">
+                        <i class="el-icon-star-off"></i>
+                        <i class="el-icon-delete"></i>
+                    </el-row>
+                </el-card>
+            </div>
+            <el-row type="flex" justify="center" style="height:50px; margin-top:40px" align="middler">
+                <el-pagination background layout="prev, pager, next"
+                :total="page.total"
+                :current-page="page.currentPage"
+                :page-size="page.pageSize"
+                @current-change="changePage" >
+                </el-pagination>
+            </el-row>
           </el-tab-pane>
           <el-tab-pane label="收藏素材" name="collect">
-              12
+            <div class="img-list">
+                <el-card class="img-card" v-for="item in list" :key="item.id">
+                    <img :src="item.url" alt="">
+                </el-card>
+            </div>
+            <el-row type="flex" justify="center" style="height:50px; margin-top:40px" align="middler">
+                    <el-pagination background layout="prev, pager, next"
+                    :total="page.total"
+                    :current-page="page.currentPage"
+                    :page-size="page.pageSize"
+                    @current-change="changePage" >
+                    </el-pagination>
+                </el-row>
           </el-tab-pane>
       </el-tabs>
   </el-card>
@@ -18,13 +46,70 @@
 export default {
   data () {
     return {
-      activeName: 'collect'
+      activeName: 'all',
+      list: [],
+      page: {
+        currentPage: 1,
+        pageSize: 15,
+        total: 0
+      }
     }
   },
-  mounted: {}
+  methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getAllMaterial()
+    },
+    changeTab () {
+      this.page.currentPage = 1
+      this.getAllMaterial()
+    },
+    getAllMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: { collect: this.activeName === 'collect',
+          page: this.page.currentPage,
+          per_page: this.page.pageSize }
+      }).then(result => {
+        this.list = result.data.results
+        this.page.total = result.data.total_count
+      })
+    }
+  },
+  created () {
+    this.getAllMaterial()
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.img-list{
+    display: flex;
+    flex-wrap: wrap;
+    .img-card{
+        width:220px;
+        height:200px;
+        margin: 10px 45px;
+        position: relative;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+        .operate{
+            width: 100%;
+            font-size: 25px;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            background-color:#ccc;
+            height: 30px;
+            i{
+                width:25px;
+                height:25px;
+                line-height: 30px;
+                color:rgb(251, 114, 153);;
+            }
+        }
+    }
+}
 </style>
